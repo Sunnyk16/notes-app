1e4206144ed6208e4e598f3e28a56bf5d7f56022
Sunnyk16 <kumarsunny12462gmail.com>
2024-03-30 03:47:45 +0530

    client added

---
+++ client/src/views/Home/Home.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+
+function Home() {
+  const [notes,setNotes] =useState([]);
+
+  const loadNotes =async ()=>{
+    const response =await axios.get('http://localhost:5000/notes');
+
+    setNotes(response.data.data);
+    console.log(response.data.data);
+
+  }
+  useEffect(()=>{
+    loadNotes();
+  },[])
+  return (
+    <div className='card card-body '>
+      {
+        notes.map((note , index)=>{
+          return(
+            <div className=' card-body  bg-red-400 m-2 p-2 w-52 capitalize  rounded-md'>
+              <h3>{note.title}</h3>
+              <p>{note.content}</p>
+              <p>{note.category}</p>
+              </div>
+          )
+        } )
+      }
+    </div>
+  )
+}
+
+export default Home
