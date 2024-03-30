02d07cf2e0f87105d4577a448dd346ea951143e6
Sunnyk16 <kumarsunny12462gmail.com>
2024-03-30 11:00:21 +0530

    file added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -4,13 +4,23 @@ import axios from 'axios';
 function Home() {
   const [notes,setNotes] =useState([]);
 
-  const loadNotes =async ()=>{
-    const response =await axios.get(`${process.env.REACT_APP_API_URL }/notes`);
+  // const loadNotes =async ()=>{
+  //   const response =await axios.get(`${process.env.REACT_APP_API_URL }/notes`);
 
-    setNotes(response.data.data);
-    console.log(response.data.data);
+  //   setNotes(response.data.data);
+  //   console.log(response.data.data);
 
+  // }
+  const loadNotes = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/notes`);
+      setNotes(response.data.data);
+      console.log(response.data.data);
+    } catch (error) {
+      console.error('Error fetching notes:', error);
+    }
   }
+  
 
   useEffect(()=>{
     loadNotes();
