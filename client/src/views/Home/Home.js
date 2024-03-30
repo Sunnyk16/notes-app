3693b312fffad318b6d0243891167d715d85fb4d
Sunnyk16 <kumarsunny12462gmail.com>
2024-03-30 10:36:00 +0530

    client env file added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -5,7 +5,7 @@ function Home() {
   const [notes,setNotes] =useState([]);
 
   const loadNotes =async ()=>{
-    const response =await axios.get('http://localhost:5000/notes');
+    const response =await axios.get(`${process.env.REACT_APP_API_URL}/notes`);
 
     setNotes(response.data.data);
     console.log(response.data.data);
